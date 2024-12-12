00444db5e08274d8f9266e04cb46de2e08ce547e
Micah Caunter <mcaunter12@gmail.com>
2024-12-12 17:14:07 -0500

    feat: move some stuff around

---
+++ ImGuiSrcGenerator/Display/Main.cs
@@ -0,0 +1,55 @@
+﻿using ImGuiNET;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImGuiSrcGenerator.Display
+{
+    public class Main : IDisplay
+    {
+        bool _toolActive = true;
+
+        public System.Numerics.Vector4 ColorV4 = Color.CornflowerBlue.ToVector4().ToNumerics();
+
+        public void Render()
+        {
+            if (_toolActive)
+            {
+                ImGui.Begin("My First Tool", ref _toolActive, ImGuiWindowFlags.MenuBar);
+                if (ImGui.BeginMenuBar())
+                {
+                    if (ImGui.BeginMenu("File"))
+                    {
+                        if (ImGui.MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+                        if (ImGui.MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
+                        if (ImGui.MenuItem("Close", "Ctrl+W")) { _toolActive = false; }
+                        ImGui.EndMenu();
+                    }
+                    ImGui.EndMenuBar();
+                }
+
+                ImGui.LabelText("Stuff", "Hello");
+
+                // Edit a color stored as 4 floats
+                ImGui.ColorEdit4("Color", ref ColorV4);
+
+                // Generate samples and plot them
+                var samples = new float[100];
+                for (var n = 0; n < samples.Length; n++)
+                    samples[n] = (float)Math.Sin(n * 0.2f + ImGui.GetTime() * 1.5f);
+                ImGui.PlotLines("Samples", ref samples[0], 100);
+
+                // Display contents in a scrolling region
+                ImGui.TextColored(new Vector4(1, 1, 0, 1).ToNumerics(), "Important Stuff");
+                ImGui.BeginChild("Scrolling", new System.Numerics.Vector2(0), ImGuiChildFlags.Border);
+                for (var n = 0; n < 50; n++)
+                    ImGui.Text($"{n:0000}: Some text");
+                ImGui.EndChild();
+                ImGui.End();
+            }
+        }
+    }
+}
--- ImGuiSrcGenerator/Game1.cs
+++ ImGuiSrcGenerator/Game1.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using MonoGame.ImGuiNet;
 using ImGuiNET;
+using ImGuiSrcGenerator.Display;
 
 namespace ImGuiSrcGenerator
 {
@@ -14,9 +15,7 @@ namespace ImGuiSrcGenerator
 
         public static ImGuiRenderer GuiRenderer;
 
-        System.Numerics.Vector4 _colorV4;
-        bool _toolActive;
-
+        Main Main = new Main();
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -29,9 +28,6 @@ namespace ImGuiSrcGenerator
         protected override void Initialize()
         {
             GuiRenderer = new ImGuiRenderer(this);
-
-            _toolActive = true;
-            _colorV4 = Color.CornflowerBlue.ToVector4().ToNumerics();
 
             base.Initialize();
         }
@@ -53,7 +49,7 @@ namespace ImGuiSrcGenerator
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(new Color(_colorV4));
+            GraphicsDevice.Clear(new Color(Main.ColorV4));
 
             _spriteBatch.Begin();
             _spriteBatch.End();
@@ -62,38 +58,7 @@ namespace ImGuiSrcGenerator
 
             GuiRenderer.BeginLayout(gameTime);
 
-            if (_toolActive)
-            {
-                ImGui.Begin("My First Tool", ref _toolActive, ImGuiWindowFlags.MenuBar);
-                if (ImGui.BeginMenuBar())
-                {
-                    if (ImGui.BeginMenu("File"))
-                    {
-                        if (ImGui.MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
-                        if (ImGui.MenuItem("Save", "Ctrl+S")) { /* Do stuff */ }
-                        if (ImGui.MenuItem("Close", "Ctrl+W")) { _toolActive = false; }
-                        ImGui.EndMenu();
-                    }
-                    ImGui.EndMenuBar();
-                }
-
-                // Edit a color stored as 4 floats
-                ImGui.ColorEdit4("Color", ref _colorV4);
-
-                // Generate samples and plot them
-                var samples = new float[100];
-                for (var n = 0; n < samples.Length; n++)
-                    samples[n] = (float)Math.Sin(n * 0.2f + ImGui.GetTime() * 1.5f);
-                ImGui.PlotLines("Samples", ref samples[0], 100);
-
-                // Display contents in a scrolling region
-                ImGui.TextColored(new Vector4(1, 1, 0, 1).ToNumerics(), "Important Stuff");
-                ImGui.BeginChild("Scrolling", new System.Numerics.Vector2(0), ImGuiChildFlags.Border | ImGuiChildFlags.AutoResizeX | ImGuiChildFlags.AutoResizeY);
-                for (var n = 0; n < 50; n++)
-                    ImGui.Text($"{n:0000}: Some text");
-                ImGui.EndChild();
-                ImGui.End();
-            }
+            Main.Render();
 
             GuiRenderer.EndLayout();
         }
